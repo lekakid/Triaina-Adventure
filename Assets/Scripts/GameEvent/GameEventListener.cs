89a87ea91efed4c1a6d0dfa7691ad2108d71519e
LeKAKiD <lekakid@kakao.com>
2021-05-08 15:17:08 +0900

    feat: 게임 이벤트 구현

---
+++ Assets/Scripts/GameEvent/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour {
+	public GameEventSO EventSO;
+
+    [SerializeField] UnityEvent EventCallback;
+
+    void OnEnable() {
+        if(EventSO == null) return;
+
+        EventSO.Register(this);
+    }
+
+    void OnDisable() {
+        if(EventSO == null) return;
+        
+        EventSO.Deregister(this);
+    }
+
+    public void InvokeCallback() {
+        EventCallback.Invoke();
+    }
+}
---
+++ Assets/Scripts/GameEvent/GameEventSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Project/GameEvent")]
+public class GameEventSO : ScriptableObject {
+    List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Register(GameEventListener listener) {
+        listeners.Add(listener);
+    }
+
+    public void Deregister(GameEventListener listener) {
+        listeners.Remove(listener);
+    }
+
+	public void Dispatch() {
+        foreach(GameEventListener listener in listeners) {
+            listener.InvokeCallback();
+        }
+    }
+}
