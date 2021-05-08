004ad253cd661dc766d7ee49b167efc9e36782ac
LeKAKiD <lekakid@kakao.com>
2021-05-08 16:16:22 +0900

    feat: 스코어 시스템 구현

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreManager", menuName = "Project/ScoreManager")]
+public class ScoreManager : ScriptableObject {
+	public int Score { get; private set; }
+    public int HighScore { get; private set; }
+
+    void Awake() {
+        HighScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    public void AddScore() {
+        Score += 1;
+        if(Score > HighScore) {
+            HighScore = Score;
+        }
+    }
+
+    public void ResetScore() {
+        Score = 0;
+    }
+
+    public void SaveHighScore() {
+        PlayerPrefs.SetInt("HighScore", HighScore);
+    }
+}
