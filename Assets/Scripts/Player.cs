db655cc148b6b8b4b5f8372c67b863a1e1d67cd9
LeKAKiD <lekakid@kakao.com>
2021-05-08 18:17:22 +0900

    feat: 플레이어 기본 동작 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [Header("Setting")]
+    [Range(1f, 100f)]
+    [SerializeField] float jumpPower = 10f;
+
+    [Header("Events")]
+    [SerializeField] GameEventSO GameOverEvent;
+
+    // Component
+    new Rigidbody2D rigidbody;
+
+    // Variable
+    Vector2 defaultPos;
+
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        defaultPos = transform.position;
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) return;
+
+        if(Input.anyKeyDown) {
+            rigidbody.velocity = Vector2.up * jumpPower;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.CompareTag("Obstacle")) {
+            GameOverEvent.Dispatch();
+        }
+    }
+
+    public void OnStart() {
+        transform.position = defaultPos;
+    }
+}
