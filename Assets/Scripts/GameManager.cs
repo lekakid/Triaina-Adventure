eef4a31d42fdf6ae63fb7502e5f9f98d67178fff
LeKAKiD <lekakid@kakao.com>
2021-05-08 18:17:55 +0900

    feat: GameManager 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [Header("Events")]
+    [SerializeField] GameEventSO PauseEvent;
+    [SerializeField] GameEventSO ResumeEvent;
+
+    [Header("Data Container")]
+	[SerializeField] ScoreManager ScoreManager;
+
+    bool isPause;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPause) {
+                Resume();
+                ResumeEvent.Dispatch();
+            }
+            else {
+                Pause();
+                PauseEvent.Dispatch();
+            }
+        }
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f;
+        isPause = true;
+    }
+
+    public void Resume() {
+        Time.timeScale = 1f;
+        isPause = false;
+    }
+
+    public void OnGameOver() {
+        ScoreManager.SaveHighScore();
+    }
+}
