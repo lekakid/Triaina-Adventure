c08d2d38661e7b65c866a7d59d75c46f64883538
LeKAKiD <lekakid@kakao.com>
2021-05-13 16:23:26 +0900

    feat: SoundManager 구현

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField] AudioClip[] BGMList;
+    [SerializeField] AudioClip[] SFXList;
+
+	AudioSource bgmSource;
+    AudioSource sfxSource;
+
+    Dictionary<string, AudioClip> bgmDic;
+    Dictionary<string, AudioClip> sfxDic;
+
+    public float BGMVolume {
+        get { return bgmSource.volume; } 
+        set { bgmSource.volume = value; }
+    }
+    public float SFXVolume {
+        get { return sfxSource.volume; } 
+        set { sfxSource.volume = value; } 
+    }
+
+    void Awake() {
+        bgmSource = gameObject.AddComponent<AudioSource>();
+        sfxSource = gameObject.AddComponent<AudioSource>();
+
+        bgmSource.volume = PlayerPrefs.GetFloat("BGM Volume", 0.5f);
+        sfxSource.volume = PlayerPrefs.GetFloat("SFX Volume", 0.5f);
+
+        bgmDic = new Dictionary<string, AudioClip>();
+        foreach(AudioClip a in BGMList) {
+            bgmDic.Add(a.name, a);
+        }
+        sfxDic = new Dictionary<string, AudioClip>();
+        foreach(AudioClip a in SFXList) {
+            sfxDic.Add(a.name, a);
+        }
+    }
+
+    void OnApplicationQuit() {
+        PlayerPrefs.SetFloat("BGM Volume", BGMVolume);
+        PlayerPrefs.SetFloat("SFX Volume", SFXVolume);
+    }
+
+    public void PlayBGM(string name) {
+        AudioClip clip;
+        if(bgmDic.TryGetValue(name, out clip)) {
+            bgmSource.clip = clip;
+            bgmSource.Play();
+        }
+    }
+
+    public void StopBGM(){
+        bgmSource.clip = null;
+        bgmSource.Stop();
+    }
+
+    public void PlaySFX(string name) {
+        AudioClip clip;
+        if(sfxDic.TryGetValue(name, out clip)) {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+}
