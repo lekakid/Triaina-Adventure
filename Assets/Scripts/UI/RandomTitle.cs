0d913efc54ebec7c82dcf0b4ac19c5e3bf8409bc
LeKAKiD <lekakid@kakao.com>
2021-05-15 21:05:50 +0900

    feat: Title, Option UI 스캐폴딩

---
+++ Assets/Scripts/UI/RandomTitle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTitle : MonoBehaviour {
+	[SerializeField] List<GameObject> TitleList;
+
+    void Awake() {
+        bool visited = PlayerPrefs.GetInt("VisitedTitle", 0) != 0;
+        
+        if(!visited) {
+            SetVisited();
+            return;
+        }
+        SetRandomTitle();
+    }
+
+    public void SetRandomTitle() {
+        foreach(GameObject o in TitleList) {
+            o.SetActive(false);
+        }
+
+        TitleList[Random.Range(0, TitleList.Count)].SetActive(true);
+    }
+
+    void SetVisited() {
+        PlayerPrefs.SetInt("VisitedTitle", 1);
+    }
+}
---
+++ Assets/Scripts/UI/UIWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWrapper : MonoBehaviour {
+    public bool onAwakeActive;
+
+    void Awake() {
+        transform.position = new Vector2(4.5f, 8f);
+        gameObject.SetActive(onAwakeActive);
+    }
+}
